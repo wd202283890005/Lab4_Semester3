5acd8601580f52b33a879ed4530d1e60ba259065
wwdd <qq1060666498@163.com>
2025-03-17 11:03:10 +0800

    directory changed name

---
+++ Project-Semester3/pe1.py
@@ -0,0 +1,8 @@
+# Prompt the user for a number
+number = float(input("Enter a number: "))
+
+# Calculate twice the entered number
+twice_number = 2 * number
+
+# Output the result
+print("Twice the entered number is:", twice_number)
